c6b6161c5eee170275bbfdd1053d62a22172fe9b
Gaurav Thapliyal <40315835+GTHAP@users.noreply.github.com>
2023-11-08 03:57:53 +0530

    Update Python_GET_Request_Test_Script.py

--- Python_GET_Request_Test_Script.py
+++ Python_GET_Request_Test_Script.py
@@ -1,18 +1,17 @@
 # This is a small script for penetration testing
 # The script sends a request to a destination IP / Host
 # The host header contains a different host
-# This being a python script and not a browser, this does not have the SNI extension in the Client Hello
+# This being a Python script and not a browser, this does not have the SNI extension in the Client Hello
+# FYI, this only works when there is a proxy in the picture and a CONNECT request is sent to connect to the proxy
 
 import requests
-
-ip = '93.184.216.34' # This is the actual destination
-
-session = requests.session()
-
-headers = {
-'host': 'www.reddit.com' # The host header contains a different destination hostname
-}
-
-proxies = { 'http': 'http://127.0.0.1:8888' } # This is required to get the traffic to a proxy such as Fiddler / Charles
-
-request = session.get(f'https://{ip}', verify=True, headers=headers, proxies=proxies) 
+ip = '20.207.73.82' # Github IP address
+s = requests.session()
+headers = {'host': 'www.example.com'}
+# When using Fiddler Proxy (to catch HTTP requests) us the three lines below and comment out lines 9 and 10
+# proxies = { 'http': 'http://127.0.0.1:8888', 'https': 'http://127.0.0.1:8888' }
+# r = s.get(f'https://{ip}', proxies=proxies, verify=False, headers=headers)
+# r1 = s.get(f'https://{ip}/explore', proxies=proxies, verify=False, headers=headers)
+r = s.get(f'https://{ip}', verify=False, headers=headers) # The URL has the Github IP and the Host header has www.example.com
+r1 = s.get(f'https://{ip}/explore', verify=False, headers=headers)
+print(r, r1)
